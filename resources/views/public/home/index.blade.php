67ba959c156234a2746a2049056a1bf9c1f35463
Alireza Bazargani <46598928+arbazargani@users.noreply.github.com>
2020-10-19 02:31:44 +0330

    Menu items changed.

--- resources/views/public/home/index.blade.php
+++ resources/views/public/home/index.blade.php
@@ -54,20 +54,23 @@
 						<a href="https://shop.cryptiner.com">
 							<li>فروشگاه</li>
 						</a>
+						<a href="http://shop.cryptiner.com/product-category/%d9%85%d8%ad%d8%b5%d9%88%d9%84%d8%a7%d8%aa/%da%a9%d8%a7%d9%86%d8%a7%d9%84-%d9%87%d8%a7%db%8c-%d8%aa%d8%b1%db%8c%d8%af-%d9%88%db%8c%da%98%d9%87/">
+							<li>کانال‌های سیگنال</li>
+						</a>
 						<a href="http://shop.cryptiner.com/product-category/%d9%85%d8%ad%d8%b5%d9%88%d9%84%d8%a7%d8%aa/%d9%85%d8%ad%d8%aa%d9%88%d8%a7%db%8c-%d8%a2%d9%85%d9%88%d8%b2%d8%b4%db%8c/">
 							<li>آکادمی</li>
 						</a>
 						<a href="#">
 							<li>مانیتورینگ</li>
 						</a>
+						<a href="http://shop.cryptiner.com/">
+							<li>تعمیرات</li>
+						</a>
 						<a href="https://blog.cryptiner.com/">
 							<li>بلاگ</li>
 						</a>
 						<a href="https://blog.cryptiner.com/%d8%aa%d9%85%d8%a7%d8%b3-%d8%a8%d8%a7-%d9%85%d8%a7/">
 							<li>تماس با ما</li>
-						</a>
-						<a href="http://shop.cryptiner.com/product-category/%d9%85%d8%ad%d8%b5%d9%88%d9%84%d8%a7%d8%aa/%da%a9%d8%a7%d9%86%d8%a7%d9%84-%d9%87%d8%a7%db%8c-%d8%aa%d8%b1%db%8c%d8%af-%d9%88%db%8c%da%98%d9%87/">
-							<li>کانال‌های سیگنال</li>
 						</a>
 						<a href="javascript:void(0);" class="toggle-icon" onclick="nav_toggle()">
 							<i class="fa fa-bars"></i>
